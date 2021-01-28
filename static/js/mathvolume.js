e53a8484a4fc0e0b7c3f3668d393480b6b86ad18
TamilVenthanRS <tamilventhanrs2003@gmail.com>
2021-01-28 10:59:33 +0000

    comments

---
+++ static/js/mathvolume.js
@@ -0,0 +1,13 @@
+CalculateBtn = document.querySelector('#button_calculate');
+
+CalculateBtn.addEventListener('click',function(e){
+    txtRADIUS = document.querySelector('#value_radius');
+    txtHEIGHT = document.querySelector('#value_height');
+    txtVOLUME = document.querySelector('#value_volume');
+
+    let volume;
+
+    volume = 3.14 * parseFloat(txtRADIUS.value) * parseFloat(txtRADIUS.value) * parseFloat(txtHEIGHT.value);
+
+    txtVOLUME.value = volume;
+});
